hollowaykeanho/Upscaler

commit d64c62f5de7f5f55c87432de601d688f11d144fe

root: fixed issues and improved formatting for powershell script
There were some issues related to output inconsistencies when
comparing with UNIX implementations and benchmark.cmd is producing
some weird timing output error (61 seconds for not a 1 minute work).

This patch fixes various issues with benchmark.cmd polygot script,
the normal execution and improved output formatting of the
init/windows.ps1 to make it more readable and understandable.

Co-authored-by: Joly0 <13993216+Joly0@users.noreply.github.com>
Co-authored-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
Signed-off-by: Joly0 <13993216+Joly0@users.noreply.github.com>
Reviewed-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

start.cmd
@@ -51,22 +51,20 @@ exit $?
 ::##############################################################################
 @echo off
 setlocal EnableDelayedExpansion
-
 IF "%*"=="" ( goto :empty )
-
 set location=%~dp0init\windows.ps1
 set location="%location%"
 set _parameters=%*
 set _parameters=!_parameters:--=-!
 set _parameters=!_parameters:input=path!
 set _parameters=!_parameters:"=\"!
-Powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%location%' %_parameters%"
+call Powershell.exe -NoProfile -Command "& '%location%' %_parameters%"
 EXIT /B
 
 :empty
 set location=%~dp0init\windows.ps1
 set location="%location%"
-Powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%location%'"
+call Powershell.exe -NoProfile -Command "& '%location%'"
 ::##############################################################################
 :: Windows Main Codes                                                          #
 ::##############################################################################

init/windows.ps1
@@ -198,21 +198,26 @@ function Invoke-Program {
 		$output_format = $subject_ext
 	}
 
-	Write-Host "Upscale Model:" $model
-	Write-Host "Upscale Scale:" $scale
-	Write-Host "Upscale Max Scale:" $model_max_scale "(0=No Limit)"
-	Write-Host "Upscale Format:" $format
-	Write-Host "Input File:" $path
+	if ($model_max_scale -eq 0 ) {
+		$model_max_scale = "unspecified"
+	}
+
+	Write-Host "Upscale Model:      " $model
+	Write-Host "Upscale Scale:      " $scale
+	Write-Host "Upscale Max Scale: " $model_max_scale
+	Write-Host "Upscale Format:     " $format
+	Write-Host "Input File:	    " $path
 	if ($video) {
-		Write-Host "Is Video Input   : 1 (0=No ; 1=Yes)"
+		Write-Host "Is Video Input:      Yes"
 	} else {
-		Write-Host "Is Video Input   : 0 (0=No ; 1=Yes)"
+		Write-Host "Is Video Input:      No"
 	}
 	Write-Host ""
-	Write-Host "Output Directory:" $subject_dir
-	Write-Host "Output Filename:" $subject_name
-	Write-Host "Output Suffix:" $subject_suffix
-	Write-Host "Output Extension:" $output_format
+	Write-Host "Output Directory:   " $subject_dir
+	Write-Host "Output Filename:    " $subject_name
+	Write-Host "Output Suffix:      " $subject_suffix
+	Write-Host "Output Extension:   " $output_format
+	Write-Host ""
 
 
 	if (-not ($video) ) {
@@ -241,25 +246,32 @@ function Invoke-Program {
 
 	if (Test-Path $control) {
 		Write-Host "Found control file $control. Restoring..."
+		Write-Host ""
 		. $control
 	} else {
 		Write-Host "Creating workspace..."
+		Write-Host ""
 		if (Test-Path $workspace) {
 			Remove-Item -Path $workspace -Recurse -Force
 		}
 		New-Item -Path $workspace -ItemType directory
 	}
 
-	Write-Host "Video Name:" "$subject_name.$subject_ext"
-	Write-Host "Video Codec:" $video_codec
-	Write-Host "Audio Codec:" $audio_codec
-	Write-Host "Pixel Format:" $pixel_format "(empty means yet to determine)"
-	Write-Host "Input Frame:" $input_frame_size
-	Write-Host "Output Frame:" $output_frame_size "(empty means yet to determine)"
+	if ($current_frame -gt $total_frames) {
+		$current_frame = $total_frames
+	}
+
+	Write-Host "Video Name:	    " "$subject_name.$subject_ext"
+	Write-Host "Video Codec:        " $video_codec
+	Write-Host "Audio Codec:        " $audio_codec
+	Write-Host "Pixel Format:       " $pixel_format "(empty means yet to determine)"
+	Write-Host "Input Frame:        " $input_frame_size
+	Write-Host "Output Frame:       " $output_frame_size "(empty means yet to determine)"
 	Write-Host ""
-	Write-Host "Frame Rate:" $frame_rate
-	Write-Host "Total Frames:" $total_frames
-	Write-Host "Current Frame:" $current_frame
+	Write-Host "Frame Rate:         " $frame_rate
+	Write-Host "Total Frames:	    " $total_frames
+	Write-Host "Current Frame:	    " $current_frame
+	Write-Host ""
 
 	$current_frame..$total_frames | ForEach-Object {
 		Write-Host "Upscaling frame" "$_/$total_frames"
@@ -298,7 +310,7 @@ function Invoke-Program {
 			-i "$path" `
 			-r $frame_rate `
 			-thread_queue_size 4096 `
-			-i "$workspace\0%d.$format" `
+			-i "$workspace\frame\0%d.$format" `
 			-c:v $video_codec `
 			-pix_fmt $pixel_format `
 			-r $frame_rate `

tests/benchmark.cmd
@@ -71,10 +71,10 @@ EXIT /B
 
 
 :runBenchmark
-del .\tests\video\sample-1-640x360-upscaled_workspace 2>null
+RMDIR /S /Q .\tests\video\sample-1-640x360-upscaled_workspace 2>null
 del .\tests\video\sample-1-640x360-upscaled.mp4 2>null
 set startTime=!time!
-start.cmd ^
+call start.cmd ^
 	--model upscayl-ultrasharp-v2 ^
 	--scale 4 ^
 	--format webp ^

commit def344cc38b9eb7ad35086b4257b67000a07935c

start.cmd: fixed execution policy restriction due to accidental removal.
For some reason, commit d64c62f5de7f5f55c87432de601d688f11d144fe
accidentally removed the bypass execution policy causing an immediate
denial of use in new Windows system. Hence, we need to restore it back.

This patch restores and fixes execution policy restriction due to
accidental removal in the previous patch.

Reported-by: Shuralyov, Jean <jean.shuralyov@proton.me>
Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
Co-authored-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

start.cmd
@@ -58,13 +58,13 @@ set _parameters=%*
 set _parameters=!_parameters:--=-!
 set _parameters=!_parameters:input=path!
 set _parameters=!_parameters:"=\"!
-call Powershell.exe -NoProfile -Command "& '%location%' %_parameters%"
+call Powershell.exe -NoProfile -executionpolicy bypass -Command "& '%location%' %_parameters%"
 EXIT /B
 
 :empty
 set location=%~dp0init\windows.ps1
 set location="%location%"
-call Powershell.exe -NoProfile -Command "& '%location%'"
+call Powershell.exe -NoProfile -executionpolicy bypass -Command "& '%location%'"
 ::##############################################################################
 :: Windows Main Codes                                                          #
 ::##############################################################################