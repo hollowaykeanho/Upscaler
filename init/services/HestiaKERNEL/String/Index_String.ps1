90848128de1b582dcb70452942e1cea64e55d023
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-11-11 13:02:47 +0800

    init: ported Index_{String,Unicode} primitive function
    Since there are a number of level 1 libraries using the string
    function, we have to port its primitive ones into HestiaKERNEL
    library. Let's do this.
    
    This patch ports Index_{String,Unicode} primitive function into
    HestiaKERNEL library into init/ directory.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ init/services/HestiaKERNEL/String/Index_String.ps1
@@ -0,0 +1,32 @@
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho’s Liberal License (the "License").
+# You must comply with the license to use the content. Get the License at:
+#
+#                 https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+. "${env:LIBS_HESTIA}\HestiaKERNEL\Unicode\Index_Unicode.ps1"
+. "${env:LIBS_HESTIA}\HestiaKERNEL\Unicode\To_Unicode_From_String.ps1"
+
+
+
+
+function HestiaKERNEL-Index-String {
+        param (
+                [string]$___input,
+                [string]$___target,
+                [string]$___from_right
+        )
+
+
+        # execute
+        $___content = HestiaKERNEL-To-Unicode-From-String $___input
+        $___chars = HestiaKERNEL-To-Unicode-From-String $___target
+
+
+        # report status
+        return HestiaKERNEL-Index-Unicode $___content $___chars $___from_right
+}
