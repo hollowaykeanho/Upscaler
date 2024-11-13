636b8abc78b829203352acd5106a2187d9952c84
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-11-13 10:05:17 +0800

    init: ported Scan_Any_Left_{String,Unicode} primitive function
    Since there are a number of level 1 libraries using the string
    function, we have to port its primitive ones into HestiaKERNEL
    library. Let's do this.
    
    This patch ports Scan_Any_Left_{String,Unicode} primitive function
    into HestiaKERNEL library into init/ directory.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ init/services/HestiaKERNEL/Unicode/Scan_Any_Left_Unicode.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho’s Liberal License (the "License").
+# You must comply with the license to use the content. Get the License at:
+#
+#                 https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+. "${LIBS_HESTIA}/HestiaKERNEL/Errors/Error_Codes.sh"
+. "${LIBS_HESTIA}/HestiaKERNEL/Unicode/Is_Unicode.sh"
+. "${LIBS_HESTIA}/HestiaKERNEL/Number/Is_Number.sh"
+
+
+
+
+HestiaKERNEL_Scan_Any_Left_Unicode() {
+        #___content_unicode="$1"
+        #___target_unicode="$2"
+        #___count="$3"
+        #___ignore="$4"
+        ___scan_index=-1
+
+
+        # validate input
+        if [ $(HestiaKERNEL_Is_Unicode "$1") -ne $HestiaKERNEL_ERROR_OK ]; then
+                printf -- ""
+                return $HestiaKERNEL_ERROR_ENTITY_EMPTY
+        fi
+
+        if [ $(HestiaKERNEL_Is_Unicode "$2") -ne $HestiaKERNEL_ERROR_OK ]; then
+                printf -- ""
+                return $HestiaKERNEL_ERROR_DATA_EMPTY
+        fi
+
+        ___count=-1
+        if [ $(HestiaKERNEL_Is_Number "$3") -eq $HestiaKERNEL_ERROR_OK ]; then
+                ___count="$3"
+        fi
+
+        ___ignore=-1
+        if [ $(HestiaKERNEL_Is_Number "$4") -eq $HestiaKERNEL_ERROR_OK ]; then
+                ___ignore="$4"
+        fi
+
+
+        # execute
+        ___list_index=""
+        ___content_unicode="$1"
+        ___target_unicode="$2"
+        ___index=0
+        while [ ! "$___content_unicode" = "" ]; do
+                # get current character
+                ___current="${___content_unicode%%, *}"
+                ___content_unicode="${___content_unicode#"$___current"}"
+                if [ "${___content_unicode%"${___content_unicode#?}"}" = "," ]; then
+                        ___content_unicode="${___content_unicode#, }"
+                fi
+
+
+                # get target character
+                ___target_unicode="$2"
+                ___scan_index=-1
+                while [ ! "$___target_unicode" = "" ]; do
+                        ___target="${___target_unicode%%, *}"
+                        ___target_unicode="${___target_unicode#"$___target"}"
+                        if [ "${___target_unicode%"${___target_unicode#?}"}" = "," ]; then
+                                ___target_unicode="${___target_unicode#, }"
+                        fi
+
+                        if [ "$___current" = "$___target" ]; then
+                                ___target_unicode=""
+                                ___scan_index="$___index"
+                                break # exit early from O(m^2) timing ASAP
+                        fi
+                done
+
+
+                # handle matches when available
+                if [ $___scan_index -ge 0 ]; then
+                        if [ $___ignore -le 0 ]; then
+                                ___list_index="${___list_index}${___scan_index}\n"
+                                if [ $___count -gt 0 ]; then
+                                        ___count=$(($___count - 1))
+                                        if [ $___count -le 0 ]; then
+                                                break
+                                        fi
+                                fi
+                        else
+                                ___ignore=$(($___ignore - 1))
+                        fi
+                fi
+
+
+                # more characters - increase index and continue
+                ___index=$(($___index + 1))
+        done
+
+
+        # report status
+        printf -- "%b" "${___list_index%"\n"}"
+        return $HestiaKERNEL_ERROR_OK
+}
